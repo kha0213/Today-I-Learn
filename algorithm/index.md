41a0aa4e9e211da353746e94f840310e3d210f4e
moon_home <tatujjang@gmail.com>
2021-01-16 10:58:37 +0900

    insert algorithm

---
+++ algorithm/index.md
@@ -0,0 +1,4 @@
+Today I Learned
+=============
+## 혼자 공부한 개념정리
+[1.HTML_01.tag](https://github.com/kha0213/Today-I-Learn/wiki/HTML_01.tag)<br>
---
+++ algorithm/src/study/ExhaustiveSearch/Lv1_PracticeTest.java
@@ -0,0 +1,20 @@
+package study.ExhaustiveSearch;
+
+/**
+ * 2021-01-16
+ * Created by tatujjang@gmail.com
+ * Github : https://github.com/kha0213/
+ * 프로그래머스 > 완전탐색 > 모의고사
+ * https://programmers.co.kr/learn/courses/30/lessons/42840
+ */
+public class Lv1_PracticeTest {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int[] answers) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
