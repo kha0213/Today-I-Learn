c744228870cf46404d2184c5cd56379bfe56e37a
kha0213 <tatujjang@gmail.com>
2021-07-13 06:55:39 +0900

    add thymeleaf

--- SpringMVC/src/main/java/com/example/springmvc/controller/BasicController.java
+++ SpringMVC/src/main/java/com/example/springmvc/controller/BasicController.java
@@ -2,17 +2,17 @@ package com.example.springmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
 public class BasicController {
@@ -50,7 +50,86 @@ public class BasicController {
 
     }
 
+    @GetMapping("/basic/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
 
+    @GetMapping("/basic/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/basic/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+    @GetMapping("/basic/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+    @GetMapping("/basic/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("emptyData", "");
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
+    @GetMapping("/basic/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/basic/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("/basic/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/basic/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/basic/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+    @Component("multiBean")
+    static class MultiBean {
+        public int mulitple(int a, int b) {
+            return a * b;
+        }
+    }
 
     @Data
     @AllArgsConstructor
