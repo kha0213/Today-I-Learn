304508ab7d328a1f5080d583a1df13b7c89f41cd
kha0213 <tatujjang@gmail.com>
2021-11-03 22:26:07 +0900

    스프링 심화과정 새로운 프로젝트 추가
    로그 찍는 프로젝트 V0 V1 V2 예제 추가

---
+++ spring-advanced/src/main/java/hello/advanced/advanced/SpringAdvancedApplication.java
@@ -0,0 +1,13 @@
+package hello.advanced.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedApplication.class, args);
+    }
+
+}
---
+++ spring-advanced/src/test/java/hello/advanced/advanced/SpringAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.advanced.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
