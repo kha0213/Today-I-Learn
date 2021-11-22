6da90fb5667ea157effa90671a343adc2e7ae663
kha0213 <tatujjang@gmail.com>
2021-11-22 20:52:19 +0900

    정리

--- mvc2/login-back/src/main/java/hello/login/web/item/ItemController.java
+++ mvc2/login-back/src/main/java/hello/login/web/item/ItemController.java
@@ -25,6 +25,7 @@ public class ItemController {
 
     @GetMapping
     public String items(Model model) {
+        //로그인 여부 체크
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "items/items";
@@ -32,6 +33,7 @@ public class ItemController {
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
+        //로그인 여부 체크
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "items/item";
@@ -39,6 +41,7 @@ public class ItemController {
 
     @GetMapping("/add")
     public String addForm(Model model) {
+        //로그인 여부 체크
         model.addAttribute("item", new Item());
         return "items/addForm";
     }
