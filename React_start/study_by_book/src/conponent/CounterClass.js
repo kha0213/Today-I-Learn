75e60500e8265cb10f8c260b1ef95de245054917
moon maria <tatujjang@gmail.com>
2022-03-16 20:17:55 +0900

    리액트를 다루는 기술 Chapter 3 end (p117)

--- React_start/study_by_book/src/conponent/CounterClass.js
+++ React_start/study_by_book/src/conponent/CounterClass.js
@@ -18,13 +18,18 @@ class CounterClass extends Component {
         const {number, fixedNumber} = this.state;
         return (
             <div>
-                <h1>{number}</h1>
+                <h1>바뀌는 값 : {number}</h1>
                 <h1>바뀌지 않는 값 : {fixedNumber}</h1>
                 <button
                     // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
                     onClick={() => {
                         // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-                        this.setState({number: number + 1})
+                        //this.setState({number: number + 1});
+                        /* setState 를 여러번 하더라도 1번만 적용된다.
+                        this.setState({number: this.state.number + 1}); */
+                        this.setState(prevState => ({
+                            number: prevState.number + 1
+                        }));
                     }}
                     >
                     +1
@@ -33,6 +38,5 @@ class CounterClass extends Component {
         );
     }
 }
-
 
 export default CounterClass;
