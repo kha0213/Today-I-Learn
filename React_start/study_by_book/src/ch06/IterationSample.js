b6ddc4eb7e80c548589e734ab1143e5c4d77ddaa
moon maria <tatujjang@gmail.com>
2022-03-21 23:06:16 +0900

    리액트를 다루는 기술 Chapter 6 end (p170)

--- React_start/study_by_book/src/ch06/IterationSample.js
+++ React_start/study_by_book/src/ch06/IterationSample.js
@@ -1,8 +1,30 @@
+import {useState} from "react";
+
 const IterationSample = () => {
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map((name) => <li key={name}>{name}</li>);
+    const [names, setNames] = useState([
+        {id: 1, text: '눈사람'},
+        {id: 2, text: '얼음'},
+        {id: 3, text: '눈'},
+        {id: 4, text: '바람'},
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+    const nameList = names.map((name) => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>);
+    const onChange = (e) => setInputText(e.target.value);
+    const onRemove = id => setNames(names.filter(name => name.id !== id));
+    const onClick = () => {
+        const nextName = {id: nextId, text: inputText};
+        //setNames(names.concat(nextName));
+        setNextId(nextId + 1);
+        setNames([...names, nextName]);
+        setInputText('');
+    }
     return (
+        <>
+        <input value={inputText} onChange={onChange}/>
+        <button onClick={onClick}>추가</button>
         <ul>{nameList}</ul>
-    );
+        </>
+    )
 }
 export default IterationSample;
