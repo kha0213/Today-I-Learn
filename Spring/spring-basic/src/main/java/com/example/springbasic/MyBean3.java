2f28e1b0069a7c0a0e9c16d1753433abec2348e2
moon maria <tatujjang@gmail.com>
2023-01-09 06:19:11 +0900

    spring-basic

--- Spring/spring-basic/src/main/java/com/example/springbasic/MyBean3.java
+++ Spring/spring-basic/src/main/java/com/example/springbasic/MyBean3.java
@@ -1,12 +1,11 @@
 package com.example.springbasic;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
-public class MyBean1 {
+public class MyBean3 {
     private final MyBean2 myBean2;
     public void print() {
-        System.out.println("MyBean1.print : " + this.getClass().getName());
+        System.out.println("MyBean3.print : " + this.getClass().getName());
     }
 }
--- Spring/spring-basic/src/main/java/com/example/springbasic/config/BasicConfiguration.java
+++ Spring/spring-basic/src/main/java/com/example/springbasic/config/BasicConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.springbasic.config;
 
+import com.example.springbasic.MyBean1;
 import com.example.springbasic.MyBean2;
+import com.example.springbasic.MyBean3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,17 @@ import org.springframework.context.annotation.Configuration;
 public class BasicConfiguration {
     @Bean
     public MyBean2 myBean2() {
+        System.out.println("BasicConfiguration.myBean2");
         return new MyBean2();
+    }
+
+    @Bean
+    public MyBean1 myBean1() {
+        return new MyBean1(myBean2());
+    }
+
+    @Bean
+    public MyBean3 myBean3() {
+        return new MyBean3(myBean2());
     }
 }
