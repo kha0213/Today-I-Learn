56a48a5928b59884e6e371f061ba295ceb31b84f
kyl0327 <kyl0327@greencar.co.kr>
2023-03-22 15:05:42 +0900

    기본 Condition학습

--- Spring/spring-boot/start/autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ Spring/spring-boot/start/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,11 +1,14 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Conditional(MemoryCondition.class)
 public class MemoryConfig {
     @Bean
     public MemoryFinder memoryFinder() {
---
+++ Spring/spring-boot/start/autoconfig/src/main/java/memory/MemoryCondition.java
@@ -0,0 +1,16 @@
+package memory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+@Slf4j
+public class MemoryCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        String memory = context.getEnvironment().getProperty("memory");
+        log.info("memory = {}", memory);
+        return "on".equals(memory);
+    }
+}
