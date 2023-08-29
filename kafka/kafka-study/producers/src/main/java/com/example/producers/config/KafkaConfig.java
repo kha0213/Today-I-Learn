4f1c880a265df42703eb756eb3f086d5a55d5c01
moon maria <tatujjang@gmail.com>
2023-08-30 00:08:47 +0900

    ack CNRK

--- kafka/kafka-study/producers/src/main/java/com/example/producers/config/KafkaConfig.java
+++ kafka/kafka-study/producers/src/main/java/com/example/producers/config/KafkaConfig.java
@@ -1,5 +1,5 @@
 package com.example.producers.config;
 
 public class KafkaConfig {
-    public static final String SERVER_PORT = "192.168.56.101:9092";
+    public static final String SERVER_PORT = "localhost:9092";
 }
